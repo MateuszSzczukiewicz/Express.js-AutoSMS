77186e01ed9311d5a14c164d9272d60e57a98967
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2024-04-26 22:08:06 +0200

    Added schedule to sms

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
 import express from "express";
+import twilio from "twilio";
+import schedule from "node-schedule";
 import cors from "cors";
 import bodyParser from "body-parser";
 
@@ -16,9 +18,39 @@ app.use(
   }),
 );
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-})
+const sendSMS = () => {
+  const now = new Date();
+
+  const date = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    21,
+    52,
+    0,
+  );
+
+  const job = schedule.scheduleJob(date, async () => {
+    try {
+      const client = new twilio.Twilio(
+        process.env.TWILIO_ACCOUNT_SID,
+        process.env.TWILIO_AUTH_TOKEN,
+      );
+      await client.messages.create({
+        body: "Hello from Twilio!",
+        from: process.env.TWILIO_PHONE_NUMBER,
+        to: process.env.PHONE_NUMBER,
+      });
+      console.log("SMS sent successfully!");
+    } catch (error) {
+      console.error("Error sending SMS:", error);
+    }
+  });
+
+  console.log("SMS scheduled at:", job.nextInvocation());
+};
+
+sendSMS();
 
 app.options("*", cors());
 
