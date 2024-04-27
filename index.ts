df9cbbdcec68fc1d2417a30c0d49dbe5e3ebe6bf
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2024-04-27 22:38:25 +0200

    Added types

--- index.ts
+++ index.ts
@@ -1,15 +1,11 @@
-import dotenv from "dotenv";
-dotenv.config({ path: ".env" });
-
-import express from "express";
+import express, { Request, Response } from "express";
 import twilio from "twilio";
-import schedule from "node-schedule";
+import schedule, { Job } from "node-schedule";
 import cors from "cors";
 import bodyParser from "body-parser";
 
 const app = express();
-
-app.use(bodyParser.json());
+let scheduledJob: Job | null = null;
 
 app.use(
   cors({
@@ -20,7 +16,19 @@ app.use(
   }),
 );
 
-app.post("/send-sms", async (req, res) => {
+// Interface for the request body of /send-sms endpoint
+interface SendSMSRequestBody {
+  message: string;
+  dayOfMonth?: string;
+  hour?: string;
+  minute?: string;
+  second?: string;
+  month?: string;
+  dayOfWeek?: string;
+}
+
+// Handling POST request for sending SMS
+app.post("/send-sms", async (req: Request, res: Response) => {
   try {
     const {
       message,
@@ -30,24 +38,26 @@ app.post("/send-sms", async (req, res) => {
       second = "*",
       month = "*",
       dayOfWeek = "*",
-    } = req.body;
+    } = req.body as SendSMSRequestBody;
 
     if (!message) {
       return res.status(400).json({ error: "Message is required." });
     }
 
+    // Creating a schedule expression based on the provided values
     const scheduleExpression = `${second} ${minute} ${hour} ${dayOfMonth} ${month} ${dayOfWeek}`;
 
-    const job = schedule.scheduleJob(scheduleExpression, async () => {
+    // Setting up a scheduled task to send SMS at the specified time and date
+    scheduledJob = schedule.scheduleJob(scheduleExpression, async () => {
       try {
         const client = new twilio.Twilio(
-          process.env.TWILIO_ACCOUNT_SID,
-          process.env.TWILIO_AUTH_TOKEN,
+          process.env.TWILIO_ACCOUNT_SID!,
+          process.env.TWILIO_AUTH_TOKEN!,
         );
         await client.messages.create({
           body: message,
-          from: process.env.TWILIO_PHONE_NUMBER,
-          to: process.env.PHONE_NUMBER,
+          from: process.env.TWILIO_PHONE_NUMBER!,
+          to: process.env.PHONE_NUMBER!,
         });
         console.log("SMS sent successfully!");
       } catch (error) {
@@ -55,7 +65,7 @@ app.post("/send-sms", async (req, res) => {
       }
     });
 
-    console.log("SMS scheduled at:", job.nextInvocation());
+    console.log("SMS scheduled at:", scheduledJob.nextInvocation());
 
     res.status(200).json({ message: "SMS has been scheduled." });
   } catch (error) {
@@ -66,7 +76,28 @@ app.post("/send-sms", async (req, res) => {
   }
 });
 
+// Canceling scheduled SMS
+app.post("/cancel-sms", async (req: Request, res: Response) => {
+  try {
+    if (!scheduledJob) {
+      return res.status(400).json({ error: "No SMS scheduled to cancel." });
+    }
+
+    scheduledJob.cancel();
+    console.log("SMS scheduled at:", scheduledJob.nextInvocation());
+
+    res.status(200).json({ message: "Scheduled SMS has been canceled." });
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while canceling the scheduled SMS." });
+  }
+});
+
 app.options("*", cors());
+
+app.use(bodyParser.json());
 
 app.listen(process.env.PORT);
 
